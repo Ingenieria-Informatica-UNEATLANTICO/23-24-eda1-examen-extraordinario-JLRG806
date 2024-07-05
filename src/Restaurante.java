817c5704263291ce647610b1aec6313a5091a827
JLRG806 <jose.rodriguez2@gmail.com>
2024-07-05 16:15:50 +0200

    feature: created small test for the new functionalities of `Hamburguesa` class.

--- src/Restaurante.java
+++ src/Restaurante.java
@@ -6,12 +6,47 @@ class Restaurante {
 
         Cocinero bobEsponja = new Cocinero();
         Camarero calamardo = new Camarero();
-        
-        bobEsponja.prepararHamburguesa();
-        calamardo.recibir(bobEsponja.entregar());
 
-        calamardo.servir();
+        bobEsponja.prepararHamburguesa();
+
+        Hamburguesa hamburguesa = bobEsponja.entregar();
+
+
+        System.out.println("Prueba de la refactorización:");
+
+        System.out.println("Hambuguesa antes de reorganizar, quitar elemento y/o agregar elementos intermendios:");
+
+        System.out.println("Sale una " + hamburguesa.describir());
+        hamburguesa.mostrar();
+        hamburguesa.describir();
+
+        System.out.println("Hamburguesa después de añadir y quitar elementos:");
+
+        hamburguesa.insertarExtra(1, new Huevo());
+        hamburguesa.insertarExtra(2, new Cheddar());
+        hamburguesa.quitarExtra(2);
+        hamburguesa.insertarExtra(2, new Mayonesa("mucho"));
+        hamburguesa.quitarExtra(4);
+        hamburguesa.insertarExtra(2, new Cheddar());
+
+
+        System.out.println("Prueba de la reorganización de elementos:");
+        hamburguesa.reorganizarExtras(3, 5);
+        System.out.println("Sale una " + hamburguesa.describir());
+        hamburguesa.mostrar();
+
+        System.out.println("Prueba de la reorganización de elementos:");
+        hamburguesa.reorganizarExtras(5, 2);
+        System.out.println("Sale una " + hamburguesa.describir());
+        hamburguesa.mostrar();
+
+
+        
+        //calamardo.recibir(bobEsponja.entregar());
+
+        //calamardo.servir();
     }
+
     public static void main(String[] args) {
         Restaurante crustaceoCrujiente = new Restaurante();
         crustaceoCrujiente.atender();
