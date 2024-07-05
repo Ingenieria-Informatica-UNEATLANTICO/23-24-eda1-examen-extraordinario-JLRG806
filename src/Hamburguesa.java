738cd4a3562cbea6d2b59f265d499240c2eaaac2
JLRG806 <jose.rodriguez2@gmail.com>
2024-07-05 16:45:04 +0200

    fix: refactoring class `Hamburguer` in usage od `ArrayList` to custom `GenericList`

--- src/Hamburguesa.java
+++ src/Hamburguesa.java
@@ -1,29 +1,28 @@
 package src;
 
-import java.util.ArrayList;
-import java.util.List;
-
 class Hamburguesa {
     private Pan panSuperior;
     private Pan panInferior;
     private Carne carne;
-    private List<Extra> extras;
+    private GenericList<Extra> extras;
 
     public Hamburguesa(Pan panSuperior, Pan panInferior, Carne carne) {
         this.panSuperior = panSuperior;
         this.panInferior = panInferior;
         this.carne = carne;
-        this.extras = new ArrayList<>();
+        this.extras = new GenericList<Extra>();
     }
 
     public void agregarExtra(Extra extra) {
-        extras.add(extra);
+        extras.insertEnd(extra);
     }
 
     public void mostrar() {
         panSuperior.mostrar();
         carne.mostrar();
-        for (Extra extra : extras) {
+
+        for (int i = 0; i < extras.size(); i++) {
+            Extra extra = extras.get(i);
             extra.mostrar();
         }
         panInferior.mostrar();
@@ -31,23 +30,28 @@ class Hamburguesa {
 
     public String describir() {
         StringBuilder descripcion = new StringBuilder(panSuperior.describir() + ", " + carne.describir());
-        for (Extra extra : extras) {
+
+        for (int i = 0; i < extras.size(); i++) {
+
+            Extra extra = extras.get(i);
+
             descripcion.append(", ").append(extra.describir());
         }
-        descripcion.append("\n");
         return descripcion.toString();
+
     }
 
     public void quitarExtra(int indice) {
 
         extras.remove(indice);
-        
+
     }
 
     public void reorganizarExtras(int indiceOrigen, int indiceDestino) {
         if (indiceOrigen >= 0 && indiceOrigen < extras.size() && indiceDestino >= 0 && indiceDestino < extras.size()) {
             Extra extra = extras.remove(indiceOrigen);
-            extras.add(indiceDestino, extra);
+            extras.insertAt(indiceDestino, extra);
+
         } else {
             throw new IndexOutOfBoundsException("Índice fuera de rango.");
         }
@@ -55,7 +59,7 @@ class Hamburguesa {
 
     public void insertarExtra(int indice, Extra extra) {
         if (indice >= 0 && indice <= extras.size()) {
-            extras.add(indice, extra);
+            extras.insertAt(indice, extra);
         } else {
             throw new IndexOutOfBoundsException("Índice fuera de rango.");
         }
