026962e682f75d73443a1f1d2ef3fc70944791c8
JLRG806 <jose.rodriguez2@gmail.com>
2024-07-05 16:44:12 +0200

    feature: added extra functiond to Generic List

--- src/GenericList.java
+++ src/GenericList.java
@@ -16,6 +16,17 @@ public class GenericList<T> {
         return count;
     }
 
+    public T get(int index) {
+        if (index < 0 || index >= this.size()) {
+            throw new IndexOutOfBoundsException("Índice fuera de rango.");
+        }
+        GenericNode<T> iterator = this.first;
+        for (int i = 0; i < index; i++) {
+            iterator = iterator.getNext();
+        }
+        return iterator.getValue();
+    }
+
     public boolean isEmpty() {
         return this.size() > 0 ? false : true;
     }
@@ -41,6 +52,44 @@ public class GenericList<T> {
             }
             iterator.setNext(newNode);
         }
+    }
+
+    public void insertAt(int index, T value) {
+        if (index < 0 || index > this.size()) {
+            throw new IndexOutOfBoundsException("Índice fuera de rango.");
+        }
+        if (index == 0) {
+            this.insertFront(value);
+        } else if (index == this.size()) {
+            this.insertEnd(value);
+        } else {
+            GenericNode<T> newNode = new GenericNode<T>(value);
+            GenericNode<T> iterator = this.first;
+            for (int i = 0; i < index - 1; i++) {
+                iterator = iterator.getNext();
+            }
+            newNode.setNext(iterator.getNext());
+            iterator.setNext(newNode);
+        }
+    }
+
+    public T remove(int index) {
+        if (index < 0 || index >= this.size()) {
+            throw new IndexOutOfBoundsException("Índice fuera de rango.");
+        }
+        T value;
+        if (index == 0) {
+            value = this.first.getValue();
+            this.first = this.first.getNext();
+        } else {
+            GenericNode<T> iterator = this.first;
+            for (int i = 0; i < index - 1; i++) {
+                iterator = iterator.getNext();
+            }
+            value = iterator.getNext().getValue();
+            iterator.setNext(iterator.getNext().getNext());
+        }
+        return value;
     }
 
     public void deleteFront() {
